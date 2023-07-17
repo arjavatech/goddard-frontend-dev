b07d28a64a3b2554ae204b0c4d7d0393b37be09f
aarthipackiyaraj <aarthi@hope3.org>
2023-07-17 18:03:33 +0530

    added js seperate file

--- js/admission_form.js
+++ js/admission_form.js
@@ -7,7 +7,8 @@ function submitForm() {
     console.log(formData);
     const obj = Object.fromEntries(formData);
     console.log(obj);
-    obj.child_id = "NCD0004";
+    obj.child_id = "NCD0005";
+    // const json=  JSON.stringify(obj);
     $.ajax({
         url: "https://y4jyv8n3cj.execute-api.us-west-2.amazonaws.com/goddard_test/admission/add",
         type: "POST",
@@ -23,65 +24,6 @@ function submitForm() {
     });
 }
 
-function namevalidation(inputtxtID,errorSpanId){
-    let regName =  /^[a-zA-Z\s]+$/;
-    if (regName.test(inputtxtID.value) == true) {
-        console.log('if');
-        document.getElementById(errorSpanId).style.display = "none";
-    } else if(inputtxtID.value == ''){
-        console.log('else if');
-        document.getElementById(errorSpanId).style.display = "none";
-    } else {
-        console.log('else');
-        document.getElementById(errorSpanId).style.display = "block";
-        inputtxtID.focus();
-    }
-}
-
-//number validation with particular format
-function validatePhone(inputtxtID, errorSpanId) {
-    let numbersformat =/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/im;
-    if (inputtxtID.value.match(numbersformat) && inputtxtID.value.length <= 15) {
-        document.getElementById(errorSpanId).style.display = "none";
-    } else if(inputtxtID.value == ''){
-        document.getElementById(errorSpanId).style.display = "none";
-    } else {
-        document.getElementById(errorSpanId).style.display = "block";
-        inputtxtID.focus();
-    }
-}
-
-//email validation with particular format
-function emailValidation(inputtxtID,errorSpanID) {
-    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    if (reg.test(inputtxtID.value) == true) {
-        document.getElementById(errorSpanID).style.display = "none";
-    } else if(inputtxtID.value == ''){
-        document.getElementById(errorSpanID).style.display = "none";
-    } else {
-        document.getElementById(errorSpanID).style.display = "block";
-        inputtxtID.focus();
-    }
-}
-
-//this function restrict future date
-function dateValidation(id){
-    document.getElementById(id).max = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().split("T")[0];
-}
-
-//pincode validation
-function ValidatePincode(inputtxt, errorSpan) {
-    var numbers = /^[0-9]+$/;
-    if (inputtxt.value.length == 5 && inputtxt.value.match(numbers)) {
-        document.getElementById(errorSpan).style.display = "none";
-    } else if(inputtxt.value == ''){
-        document.getElementById(errorSpan).style.display = "none";
-    } else {
-        document.getElementById(errorSpan).style.display = "inline";
-        inputtxt.focus();
-    }
-}
-
 $(document).ready(function() {
     if(!isAuthenticated()) {
         window.location.href = 'login.html';
@@ -93,10 +35,6 @@ $(document).ready(function() {
             e.preventDefault(); // Prevent the default form submission
             submitForm();
         });
-        namevalidation();
-        validatePhone();
-        emailValidation();
-        dateValidation();
     }
 });
 

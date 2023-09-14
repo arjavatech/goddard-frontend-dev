6207d0c26355ff3bf4db4f85206f4cd995f8678e
aarthipackiyaraj <aarthi@hope3.org>
2023-09-14 18:33:42 +0530

    admin dashboard table details send to parent invite table

--- js/inviteParentEmail.js
+++ js/inviteParentEmail.js
@@ -11,6 +11,7 @@ const s3 = new AWS.S3();
 var parent_email;
 let email = $('#parent_email').val();
 console.log(email);
+let admission_form = document.getElementById('admission_form');
 let obj = {
     "from": "noreply.goddard@gmail.com",
     "to": `${email}`,
@@ -22,21 +23,45 @@ console.log(obj);
 async function emailSend() {
     try {
         obj.subject = 'invite parent';
-        let parent_email = $('#parent_email').val();
-        console.log(parent_email);
+        let parent_email = $('#parent_one_email').val();
         obj.to =parent_email;
         let messageData = $('#messageData').val();
-        console.log(messageData);
         obj.body = messageData;
         console.log(obj);
+        const json =JSON.stringify(obj);
+        console.log(json);
+
+
+        const form = document.getElementById("admission_form");
+        console.log(form);
+        const formData = new FormData(form);
+        console.log(formData);
+        // const inviteParentData = new FormData(formData);
+        const inviteParentData = Object.fromEntries(formData);
+        console.log(inviteParentData);
+        const json1 = JSON.stringify(inviteParentData);
+        console.log(json1);
+
         $.ajax({
                url: "https://y4jyv8n3cj.execute-api.us-west-2.amazonaws.com/goddard_test/email/send",
-               type: "OPTIONS",
+               type: "Options",
                contentType: "application/json",
-               data: JSON.stringify(obj),
+               data: json,
                success: function (response) {
                    alert("Email Sent Successfully")
-                   window.location.href = "admin_dashboard.html";
+                   $.ajax({
+                    url: "https://y4jyv8n3cj.execute-api.us-west-2.amazonaws.com/goddard_test/invite_info/add",
+                    type: "POST",
+                    contentType: "application/json",
+                    data: json1,
+                    success: function (response1) {
+                        alert("data submitted successfully");
+                        window.location.href = "admin_dashboard.html";
+                    },
+                    error: function (xhr, status, error) {
+                        alert("form submit failed");
+                    }
+                });
                },
                error: function (xhr, status, error) {
                    alert("Email sending failed")

63749487e3f3543d99665512d8c9c91093df9dbf
aarthipackiyaraj <aarthi@hope3.org>
2023-09-27 17:24:34 +0530

    added multi select checkbox

--- js/admin_dashboard.js
+++ js/admin_dashboard.js
@@ -1,182 +1,161 @@
-import {signOut,isAuthenticated} from "./authenticationVerify.js";
+// import {signOut,isAuthenticated} from "./authenticationVerify.js";
 
-// Signout button event listener
-document.getElementById('btn-signout').addEventListener('click', signOut);
+// // Signout button event listener
+// document.getElementById('btn-signout').addEventListener('click', signOut);
 
-// Function to submit the form data
-function saveForm() {
+// // Function to submit the form data
+// function saveForm() {
 
-    const form = document.getElementById("admission_form");
-    const formData = new FormData(form);
-    const obj = Object.fromEntries(formData);
-    obj.year = new Date().getFullYear() + '';
-    console.log(obj);
-    // const json=  JSON.stringify(obj);
-    // console.log(json);
-    $.ajax({
-        url: "https://y4jyv8n3cj.execute-api.us-west-2.amazonaws.com/goddard_test/parent_invite_info/add",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(obj),
-        success: function (response) {
-            console.log(response);
-            // window.location.href = "admin_dashboard.html";
-            // alert("form submitted successfully");
-        },
-        error: function (xhr, status, error) {
-            console.log(error);
-            alert("form submit failed");
-        }
-    });
-}
+//     const form = document.getElementById("admission_form");
+//     const formData = new FormData(form);
+//     const obj = Object.fromEntries(formData);
+//     obj.year = new Date().getFullYear() + '';
+//     console.log(obj);
+//     // const json=  JSON.stringify(obj);
+//     // console.log(json);
+//     $.ajax({
+//         url: "https://y4jyv8n3cj.execute-api.us-west-2.amazonaws.com/goddard_test/parent_invite_info/add",
+//         type: "POST",
+//         contentType: "application/json",
+//         data: JSON.stringify(obj),
+//         success: function (response) {
+//             console.log(response);
+//             // window.location.href = "admin_dashboard.html";
+//             // alert("form submitted successfully");
+//         },
+//         error: function (xhr, status, error) {
+//             console.log(error);
+//             alert("form submit failed");
+//         }
+//     });
+// }
 
-function activeFormList(){
-    console.log('dvsdfv');
-    $.ajax({
-        url :'https://y4jyv8n3cj.execute-api.us-west-2.amazonaws.com/goddard_test/goddard_all_form/all',
-        type : 'GET',
-        success : function(response){
-            console.log(response);
-            let responsevalue = Object.values(response);
-            console.log(responsevalue);
-            let optionsData = '';
-            console.log(document.querySelector('[name="active_form_list"]').value);
-            // document.querySelector('[name="active_form_list"]').value ='';
-            for (let i = 0; i <= responsevalue.length; i++) {
-                console.log(responsevalue[i].form_name);
-                optionsData += '<option value="' + responsevalue[i] + '">' + responsevalue[i]
-                                + '</option>';
-                document.querySelector('[name="active_form_list"]').value =
-                    optionsData;
-            }
-        }
-    });
-}
 
-$(document).ready(function () {
-    if (!isAuthenticated()) {
-        window.location.href = 'login.html';
-    } else {
-        document.body.style.visibility = 'visible';
-        $("#saveButton").on("click", function (e) {
-            e.preventDefault(); // Prevent the default form submission
-            const saveButton = document.getElementById("saveButton");
-            saveButton.disabled = 'true';
-            const emailButton = document.getElementById("emailButtonDiv");
-            console.log(emailButton);
-            emailButton.style.display ="block";
-            saveForm();
-        });
-        $("#active_form_list").on("click", function (e) {
-            e.preventDefault();
-            console.log('checking');
-             activeFormList();
-        });
-    }
-    jQuery(document).ready(function () {
-        // click on next button
-        jQuery('.form-wizard-next-btn').click(function () {
-            var parentFieldset = jQuery(this).parents('.wizard-fieldset');
-            var currentActiveStep = jQuery(this).parents('.form-wizard')
-                .find('.form-wizard-steps .active');
-            var next = jQuery(this);
-            var nextWizardStep = true;
-            parentFieldset.find('.wizard-required').each(function () {
-                var thisValue = jQuery(this).val();
+
+// $(document).ready(function () {
+//     if (!isAuthenticated()) {
+//         window.location.href = 'login.html';
+//     } else {
+//         document.body.style.visibility = 'visible';
+//         $("#saveButton").on("click", function (e) {
+//             e.preventDefault(); // Prevent the default form submission
+//             const saveButton = document.getElementById("saveButton");
+//             saveButton.disabled = 'true';
+//             const emailButton = document.getElementById("emailButtonDiv");
+//             console.log(emailButton);
+//             emailButton.style.display ="block";
+//             saveForm();
+//         });
+//         // $("#active_form_list").on("click", function (e) {
+//         //     e.preventDefault();
+//         //     console.log('checking');
+//         //      activeFormList();
+//         // });
+//     }
+//     jQuery(document).ready(function () {
+//         // click on next button
+//         jQuery('.form-wizard-next-btn').click(function () {
+//             var parentFieldset = jQuery(this).parents('.wizard-fieldset');
+//             var currentActiveStep = jQuery(this).parents('.form-wizard')
+//                 .find('.form-wizard-steps .active');
+//             var next = jQuery(this);
+//             var nextWizardStep = true;
+//             parentFieldset.find('.wizard-required').each(function () {
+//                 var thisValue = jQuery(this).val();
     
-                if (thisValue == "") {
-                    jQuery(this).siblings(".wizard-form-error").slideDown();
-                    nextWizardStep = false;
-                }
-                else {
-                    jQuery(this).siblings(".wizard-form-error").slideUp();
-                }
-            });
-            if (nextWizardStep) {
-                next.parents('.wizard-fieldset').removeClass("show", "400");
-                currentActiveStep.removeClass('active').addClass('activated').next()
-                    .addClass('active', "400");
-                next.parents('.wizard-fieldset').next('.wizard-fieldset').addClass("show", "400");
-                jQuery(document).find('.wizard-fieldset').each(function () {
-                    if (jQuery(this).hasClass('show')) {
-                        var formAtrr = jQuery(this).attr('data-tab-content');
-                        jQuery(document).find('.form-wizard-steps .form-wizard-step-item')
-                            .each(function () {
-                                if (jQuery(this).attr('data-attr') == formAtrr) {
-                                    jQuery(this).addClass('active');
-                                    var innerWidth = jQuery(this).innerWidth();
-                                    var position = jQuery(this).position();
-                                    jQuery(document).find('.form-wizard-step-move')
-                                        .css({"left": position.left, "width": innerWidth});
-                                } else {
-                                    jQuery(this).removeClass('active');
-                                }
-                            });
-                    }
-                });
-            }
-        });
-        //click on previous button
-        jQuery('.form-wizard-previous-btn').click(function () {
-            var counter = parseInt(jQuery(".wizard-counter").text());
-            ;
-            var prev = jQuery(this);
-            var currentActiveStep = jQuery(this).parents('.form-wizard')
-                .find('.form-wizard-steps .active');
-            prev.parents('.wizard-fieldset').removeClass("show", "400");
-            prev.parents('.wizard-fieldset').prev('.wizard-fieldset').addClass("show", "400");
-            currentActiveStep.removeClass('active').prev().removeClass('activated')
-                .addClass('active', "400");
-            jQuery(document).find('.wizard-fieldset').each(function () {
-                if (jQuery(this).hasClass('show')) {
-                    var formAtrr = jQuery(this).attr('data-tab-content');
-                    jQuery(document).find('.form-wizard-steps .form-wizard-step-item')
-                        .each(function () {
-                            if (jQuery(this).attr('data-attr') == formAtrr) {
-                                jQuery(this).addClass('active');
-                                var innerWidth = jQuery(this).innerWidth();
-                                var position = jQuery(this).position();
-                                jQuery(document).find('.form-wizard-step-move')
-                                    .css({"left": position.left, "width": innerWidth});
-                            } else {
-                                jQuery(this).removeClass('active');
-                            }
-                        });
-                }
-            });
-        });
-        //click on form submit button
-        jQuery(document).on("click", ".form-wizard .form-wizard-submit", function () {
-            var parentFieldset = jQuery(this).parents('.wizard-fieldset');
-            var currentActiveStep = jQuery(this).parents('.form-wizard')
-                .find('.form-wizard-steps .active');
-            parentFieldset.find('.wizard-required').each(function () {
-                var thisValue = jQuery(this).val();
-                if (thisValue == "") {
-                    jQuery(this).siblings(".wizard-form-error").slideDown();
-                } else {
-                    jQuery(this).siblings(".wizard-form-error").slideUp();
-                }
-            });
-        });
-        // focus on input field check empty or not
-        jQuery(".form-control").on('focus', function () {
-            var tmpThis = jQuery(this).val();
-            if (tmpThis == '') {
-                jQuery(this).parent().addClass("focus-input");
-            } else if (tmpThis != '') {
-                jQuery(this).parent().addClass("focus-input");
-            }
-        }).on('blur', function () {
-            var tmpThis = jQuery(this).val();
-            if (tmpThis == '') {
-                jQuery(this).parent().removeClass("focus-input");
-                jQuery(this).siblings('.wizard-form-error').slideDown("3000");
-            } else if (tmpThis != '') {
-                jQuery(this).parent().addClass("focus-input");
-                jQuery(this).siblings('.wizard-form-error').slideUp("3000");
-            }
-        });
-    });
-});
+//                 if (thisValue == "") {
+//                     jQuery(this).siblings(".wizard-form-error").slideDown();
+//                     nextWizardStep = false;
+//                 }
+//                 else {
+//                     jQuery(this).siblings(".wizard-form-error").slideUp();
+//                 }
+//             });
+//             if (nextWizardStep) {
+//                 next.parents('.wizard-fieldset').removeClass("show", "400");
+//                 currentActiveStep.removeClass('active').addClass('activated').next()
+//                     .addClass('active', "400");
+//                 next.parents('.wizard-fieldset').next('.wizard-fieldset').addClass("show", "400");
+//                 jQuery(document).find('.wizard-fieldset').each(function () {
+//                     if (jQuery(this).hasClass('show')) {
+//                         var formAtrr = jQuery(this).attr('data-tab-content');
+//                         jQuery(document).find('.form-wizard-steps .form-wizard-step-item')
+//                             .each(function () {
+//                                 if (jQuery(this).attr('data-attr') == formAtrr) {
+//                                     jQuery(this).addClass('active');
+//                                     var innerWidth = jQuery(this).innerWidth();
+//                                     var position = jQuery(this).position();
+//                                     jQuery(document).find('.form-wizard-step-move')
+//                                         .css({"left": position.left, "width": innerWidth});
+//                                 } else {
+//                                     jQuery(this).removeClass('active');
+//                                 }
+//                             });
+//                     }
+//                 });
+//             }
+//         });
+//         //click on previous button
+//         jQuery('.form-wizard-previous-btn').click(function () {
+//             var counter = parseInt(jQuery(".wizard-counter").text());
+//             ;
+//             var prev = jQuery(this);
+//             var currentActiveStep = jQuery(this).parents('.form-wizard')
+//                 .find('.form-wizard-steps .active');
+//             prev.parents('.wizard-fieldset').removeClass("show", "400");
+//             prev.parents('.wizard-fieldset').prev('.wizard-fieldset').addClass("show", "400");
+//             currentActiveStep.removeClass('active').prev().removeClass('activated')
+//                 .addClass('active', "400");
+//             jQuery(document).find('.wizard-fieldset').each(function () {
+//                 if (jQuery(this).hasClass('show')) {
+//                     var formAtrr = jQuery(this).attr('data-tab-content');
+//                     jQuery(document).find('.form-wizard-steps .form-wizard-step-item')
+//                         .each(function () {
+//                             if (jQuery(this).attr('data-attr') == formAtrr) {
+//                                 jQuery(this).addClass('active');
+//                                 var innerWidth = jQuery(this).innerWidth();
+//                                 var position = jQuery(this).position();
+//                                 jQuery(document).find('.form-wizard-step-move')
+//                                     .css({"left": position.left, "width": innerWidth});
+//                             } else {
+//                                 jQuery(this).removeClass('active');
+//                             }
+//                         });
+//                 }
+//             });
+//         });
+//         //click on form submit button
+//         jQuery(document).on("click", ".form-wizard .form-wizard-submit", function () {
+//             var parentFieldset = jQuery(this).parents('.wizard-fieldset');
+//             var currentActiveStep = jQuery(this).parents('.form-wizard')
+//                 .find('.form-wizard-steps .active');
+//             parentFieldset.find('.wizard-required').each(function () {
+//                 var thisValue = jQuery(this).val();
+//                 if (thisValue == "") {
+//                     jQuery(this).siblings(".wizard-form-error").slideDown();
+//                 } else {
+//                     jQuery(this).siblings(".wizard-form-error").slideUp();
+//                 }
+//             });
+//         });
+//         // focus on input field check empty or not
+//         jQuery(".form-control").on('focus', function () {
+//             var tmpThis = jQuery(this).val();
+//             if (tmpThis == '') {
+//                 jQuery(this).parent().addClass("focus-input");
+//             } else if (tmpThis != '') {
+//                 jQuery(this).parent().addClass("focus-input");
+//             }
+//         }).on('blur', function () {
+//             var tmpThis = jQuery(this).val();
+//             if (tmpThis == '') {
+//                 jQuery(this).parent().removeClass("focus-input");
+//                 jQuery(this).siblings('.wizard-form-error').slideDown("3000");
+//             } else if (tmpThis != '') {
+//                 jQuery(this).parent().addClass("focus-input");
+//                 jQuery(this).siblings('.wizard-form-error').slideUp("3000");
+//             }
+//         });
+//     });
+// });
 

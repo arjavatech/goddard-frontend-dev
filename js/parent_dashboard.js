6e5764dbc52adfb81c6f1b45a53fbaa5d62fe1ee
varsha-arjava <varsha.arjava@gmail.com>
2025-02-20 22:03:16 +0530

    auth and entrol form alignment issue fix

--- js/parent_dashboard.js
+++ js/parent_dashboard.js
@@ -379,6 +379,11 @@ function checking(editID) {
         },
         x: 12,
         y: 12,
+        autoPaging: "slice",
+        html2canvas: {
+          scale: 0.75,
+        },
+        pagesplit: true,
       });
     });
   }
@@ -1070,20 +1075,20 @@ function checking(editID) {
             console.log(window.location.origin);
             switch (full.formname) {
               case "admission_form":
-                url = `${window.location.origin}/goddard-frontend-dev/admission_form_completed.html?id=${editID}`;
-                print_url = `${window.location.origin}/goddard-frontend-dev/admission_form_pdf_completed.html?id=${editID}`;
+                url = `${window.location.origin}/admission_form_completed.html?id=${editID}`;
+                print_url = `${window.location.origin}/admission_form_pdf_completed.html?id=${editID}`;
                 break;
               case "authorization_form":
-                url = `${window.location.origin}/goddard-frontend-dev/authorization_completed.html?id=${editID}`;
-                print_url = `${window.location.origin}/goddard-frontend-dev/authorization_pdf_completed.html?id=${editID}`;
+                url = `${window.location.origin}/authorization_completed.html?id=${editID}`;
+                print_url = `${window.location.origin}/authorization_pdf_completed.html?id=${editID}`;
                 break;
               case "enrollment_form":
-                url = `${window.location.origin}/goddard-frontend-dev/enrollment_agreement_completed.html?id=${editID}`;
-                print_url = `${window.location.origin}/goddard-frontend-dev/enrollment_agreement_pdf_completed.html?id=${editID}`;
+                url = `${window.location.origin}/enrollment_agreement_completed.html?id=${editID}`;
+                print_url = `${window.location.origin}/enrollment_agreement_pdf_completed.html?id=${editID}`;
                 break;
               case "parent_handbook":
-                url = `${window.location.origin}/goddard-frontend-dev/parent_handbook_completed.html?id=${editID}`;
-                print_url = `${window.location.origin}/goddard-frontend-dev/parent_handbook_pdf_completed.html?id=${editID}`;
+                url = `${window.location.origin}/parent_handbook_completed.html?id=${editID}`;
+                print_url = `${window.location.origin}/parent_handbook_pdf_completed.html?id=${editID}`;
                 break;
               default:
                 return "";

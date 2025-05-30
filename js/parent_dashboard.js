2ecf85b1ea8e1f07988e226dd365127e4211a104
varsha-arjava <varsha.arjava@gmail.com>
2025-05-30 12:48:58 +0530

    just changed local to cloud alone(parent_dashboard)

--- js/parent_dashboard.js
+++ js/parent_dashboard.js
@@ -1203,48 +1203,48 @@ function completedForm(editID, curyear) {
                     // console.log(window.location.origin);
 
                     // for localhost 
-                    switch (full.formname) {
-                        case "admission_form":
-                            url = `${window.location.origin}/admission_form_completed.html?id=${editID}`;
-                            print_url = `${window.location.origin}/admission_form_pdf_completed.html?id=${editID}`;
-                            break;
-                        case "authorization_form":
-                            url = `${window.location.origin}/authorization_completed.html?id=${editID}`;
-                            print_url = `${window.location.origin}/authorization_pdf_completed.html?id=${editID}`;
-                            break;
-                        case "enrollment_form":
-                            url = `${window.location.origin}/enrollment_agreement_completed.html?id=${editID}`;
-                            print_url = `${window.location.origin}/enrollment_agreement_pdf_completed.html?id=${editID}`;
-                            break;
-                        case "parent_handbook":
-                            url = `${window.location.origin}/parent_handbook_completed.html?id=${editID}`;
-                            print_url = `${window.location.origin}/parent_handbook_pdf_completed.html?id=${editID}`;
-                            break;
-                        default:
-                            return "";
-                    }
-
-                    //  for cloud
                     // switch (full.formname) {
                     //     case "admission_form":
-                    //         url = `${window.location.origin}/goddard-frontend-dev/admission_form_completed.html?id=${editID}`;
-                    //         print_url = `${window.location.origin}/goddard-frontend-dev/admission_form_pdf_completed.html?id=${editID}`;
+                    //         url = `${window.location.origin}/admission_form_completed.html?id=${editID}`;
+                    //         print_url = `${window.location.origin}/admission_form_pdf_completed.html?id=${editID}`;
                     //         break;
                     //     case "authorization_form":
-                    //         url = `${window.location.origin}/goddard-frontend-dev/authorization_completed.html?id=${editID}`;
-                    //         print_url = `${window.location.origin}/goddard-frontend-dev/authorization_pdf_completed.html?id=${editID}`;
+                    //         url = `${window.location.origin}/authorization_completed.html?id=${editID}`;
+                    //         print_url = `${window.location.origin}/authorization_pdf_completed.html?id=${editID}`;
                     //         break;
                     //     case "enrollment_form":
-                    //         url = `${window.location.origin}/goddard-frontend-dev/enrollment_agreement_completed.html?id=${editID}`;
-                    //         print_url = `${window.location.origin}/goddard-frontend-dev/enrollment_agreement_pdf_completed.html?id=${editID}`;
+                    //         url = `${window.location.origin}/enrollment_agreement_completed.html?id=${editID}`;
+                    //         print_url = `${window.location.origin}/enrollment_agreement_pdf_completed.html?id=${editID}`;
                     //         break;
                     //     case "parent_handbook":
-                    //         url = `${window.location.origin}/goddard-frontend-dev/parent_handbook_completed.html?id=${editID}`;
-                    //         print_url = `${window.location.origin}/goddard-frontend-dev/parent_handbook_pdf_completed.html?id=${editID}`;
+                    //         url = `${window.location.origin}/parent_handbook_completed.html?id=${editID}`;
+                    //         print_url = `${window.location.origin}/parent_handbook_pdf_completed.html?id=${editID}`;
                     //         break;
                     //     default:
                     //         return "";
                     // }
+
+                    //  for cloud
+                    switch (full.formname) {
+                        case "admission_form":
+                            url = `${window.location.origin}/goddard-frontend-dev/admission_form_completed.html?id=${editID}`;
+                            print_url = `${window.location.origin}/goddard-frontend-dev/admission_form_pdf_completed.html?id=${editID}`;
+                            break;
+                        case "authorization_form":
+                            url = `${window.location.origin}/goddard-frontend-dev/authorization_completed.html?id=${editID}`;
+                            print_url = `${window.location.origin}/goddard-frontend-dev/authorization_pdf_completed.html?id=${editID}`;
+                            break;
+                        case "enrollment_form":
+                            url = `${window.location.origin}/goddard-frontend-dev/enrollment_agreement_completed.html?id=${editID}`;
+                            print_url = `${window.location.origin}/goddard-frontend-dev/enrollment_agreement_pdf_completed.html?id=${editID}`;
+                            break;
+                        case "parent_handbook":
+                            url = `${window.location.origin}/goddard-frontend-dev/parent_handbook_completed.html?id=${editID}`;
+                            print_url = `${window.location.origin}/goddard-frontend-dev/parent_handbook_pdf_completed.html?id=${editID}`;
+                            break;
+                        default:
+                            return "";
+                    }
                     return `
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="18" height="18" class="action-icons m-2 download-btn" data-url="${url}" data-name="${full.formname}.pdf" name="downbutton"><path fill="#0F2D52" d="M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM376.9 294.6L269.8 394.5c-3.8 3.5-8.7 5.5-13.8 5.5s-10.1-2-13.8-5.5L135.1 294.6c-4.5-4.2-7.1-10.1-7.1-16.3c0-12.3 10-22.3 22.3-22.3l57.7 0 0-96c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 96 57.7 0c12.3 0 22.3 10 22.3 22.3c0 6.2-2.6 12.1-7.1 16.3z"/></svg>
